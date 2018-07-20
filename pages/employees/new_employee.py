cb52bde90d12f75487fef534a733fb3f8b781db8
bilalikram94 <bilalikram94@gmail.com>
2018-07-20 19:54:05 +0500

    commit

--- pages/employees/new_employee.py
+++ pages/employees/new_employee.py
@@ -5,6 +5,8 @@ import logging
 
 class NewEmployee(BasePage):
     log = cl.customLogger(logging.DEBUG)
+    locator = None
+    locatorType = None
 
     # Locators
     _first_name = "//main[@id='wrapper']//div[@class='container']/div[@class='row']//form[@role='form']//input[@name='fname']"  # Xpath
@@ -23,75 +25,142 @@ class NewEmployee(BasePage):
     def __init__(self, driver):
         super().__init__(driver)
 
-    def verifyFirstName(self):
-        result = self.isElementPresent(self._first_name, locatorType='xpath')
+    def verifyFirstName(self, newemp, name):
+        db = self.dbGetElement(newemp, name)
+        try:
+            self.locator = db[0]
+            self.locatorType = db[1]
+        except:
+            self.log.error("Something went wrong")
+        result = self.isElementPresent(self.locator, self.locatorType)
         return result
 
-    def verifyLastName(self):
-        result = self.isElementPresent(self._last_name, locatorType='xpath')
+    def verifyLastName(self, newemp1, lname):
+        db = self.dbGetElement(newemp1, lname)
+        try:
+            self.locator = db[0]
+            self.locatorType = db[1]
+        except:
+            self.log.error("Something went wrong")
+        result = self.isElementPresent(self.locator, self.locatorType)
         return result
 
-    def verifyEmployeeCard(self):
-        result = self.isElementPresent(self._employee_card, locatorType='xpath')
+    def verifyEmployeeCard(self, newemp2, empcard):
+        db = self.dbGetElement(newemp2, empcard)
+        try:
+            self.locator = db[0]
+            self.locatorType = db[1]
+        except:
+            self.log.error("Something went wrong")
+        result = self.isElementPresent(self.locator, self.locatorType)
         return result
 
-    def verifyGender(self):
-        result = self.isElementPresent(self._gender, locatorType='xpath')
+    def verifyGender(self, newemp3, gender):
+        db = self.dbGetElement(newemp3, gender)
+        try:
+            self.locator = db[0]
+            self.locatorType = db[1]
+        except:
+            self.log.error("Something went wrong")
+        result = self.isElementPresent(self.locator, self.locatorType)
         return result
 
-    def verifyDateOfJoining(self):
-        result = self.isElementPresent(self._date_of_joining, locatorType='xpath')
+    def verifyDateOfJoining(self, newemp4, doj):
+        db = self.dbGetElement(newemp4, doj)
+        try:
+            self.locator = db[0]
+            self.locatorType = db[1]
+        except:
+            self.log.error("Something went wrong")
+        result = self.isElementPresent(self.locator, self.locatorType)
         return result
 
-    def verifyDepartment(self):
-        result = self.isElementPresent(self._department)
+    def verifyDepartment(self, newemp5, depart):
+        db = self.dbGetElement(newemp5, depart)
+        try:
+            self.locator = db[0]
+            self.locatorType = db[1]
+        except:
+            self.log.error("Something went wrong")
+        result = self.isElementPresent(self.locator, self.locatorType)
         return result
 
-    def verifyDesignation(self):
-        result = self.isElementPresent(self._designation)
+    def verifyDesignation(self, newemp6, design):
+        db = self.dbGetElement(newemp6, design)
+        try:
+            self.locator = db[0]
+            self.locatorType = db[1]
+        except:
+            self.log.error("Something went wrong")
+        result = self.isElementPresent(self.locator, self.locatorType)
         return result
 
-    def verifyReportingPerson(self):
-        result = self.isElementPresent(self._reporting_person, locatorType='css')
+    def verifyReportingPerson(self, newemp7, report):
+        db = self.dbGetElement(newemp7, report)
+        try:
+            self.locator = db[0]
+            self.locatorType = db[1]
+        except:
+            self.log.error("Something went wrong")
+        result = self.isElementPresent(self.locator, self.locatorType)
         return result
 
-    def verifyTaxRule(self):
-        result = self.isElementPresent(self._tax_rule, locatorType='css')
+    def verifyTaxRule(self, newemp8, tax):
+        db = self.dbGetElement(newemp8, tax)
+        try:
+            self.locator = db[0]
+            self.locatorType = db[1]
+        except:
+            self.log.error("Something went wrong")
+        result = self.isElementPresent(self.locator, self.locatorType)
         return result
 
-    def verifyUserRole(self):
-        self.scrollIntoView(self._user_role, locatorType='css')
+    def verifyUserRole(self, newemp9, urole):
+        db = self.dbGetElement(newemp9, urole)
+        try:
+            self.locator = db[0]
+            self.locatorType = db[1]
+        except:
+            self.log.error("Something went wrong")
+        self.scrollIntoView(self.locator, self.locatorType)
         self.util.sleep(3)
-        result = self.isElementPresent(self._user_role, locatorType='css')
+        result = self.isElementPresent(self.locator, self.locatorType)
         return result
 
-    def verifyPolicyType(self):
-        self.scrollIntoView(self._policy_type, locatorType='css')
-        result = self.isElementPresent(self._policy_type, locatorType='css')
+    def verifyPolicyType(self, newemp10, policy):
+        db = self.dbGetElement(newemp10, policy)
+        try:
+            self.locator = db[0]
+            self.locatorType = db[1]
+        except:
+            self.log.error("Something went wrong")
+        self.scrollIntoView(self.locator, self.locatorType)
+        result = self.isElementPresent(self.locator, self.locatorType)
         return result
 
-    def NewEmployeeSmoke(self):
+    def NewEmployeeSmoke(self, newemp, name, newemp1, lname, newemp2, empcard, newemp3, gender, newemp4, doj, newemp5,
+                         depart, newemp6, design, newemp7, report, newemp8, tax, newemp9, urole, newemp10, policy):
         self.nav.NewEmployee()
         self.verifyPageTitle(self._page_title)
-        result = self.verifyFirstName()
+        result = self.verifyFirstName(newemp, name)
         self.stat.mark(result, "Verify First Name")
-        result1 = self.verifyLastName()
+        result1 = self.verifyLastName(newemp1, lname)
         self.stat.mark(result1, "Verify Last Name")
-        result2 = self.verifyEmployeeCard()
+        result2 = self.verifyEmployeeCard(newemp2, empcard)
         self.stat.mark(result2, "Verify Employee Card")
-        result3 = self.verifyGender()
+        result3 = self.verifyGender(newemp3, gender)
         self.stat.mark(result3, "Verify Gender")
-        result4 = self.verifyDateOfJoining()
+        result4 = self.verifyDateOfJoining(newemp4, doj)
         self.stat.mark(result4, "Verify Date Of Joining")
-        result5 = self.verifyDepartment()
+        result5 = self.verifyDepartment(newemp5, depart)
         self.stat.mark(result5, "Verify Department")
-        result6 = self.verifyDesignation()
+        result6 = self.verifyDesignation(newemp6, design)
         self.stat.mark(result6, "Verify Designation")
-        result7 = self.verifyReportingPerson()
+        result7 = self.verifyReportingPerson(newemp7, report)
         self.stat.mark(result7, "Verify Reporting Person")
-        result8 = self.verifyTaxRule()
+        result8 = self.verifyTaxRule(newemp8, tax)
         self.stat.mark(result8, "Verify Tax Rule")
-        result9 = self.verifyUserRole()
+        result9 = self.verifyUserRole(newemp9, urole)
         self.stat.mark(result9, "Verify User Role")
-        result10 = self.verifyPolicyType()
+        result10 = self.verifyPolicyType(newemp10, policy)
         self.stat.markFinal("Test New Employee", result10, "Verify Policy Type")
--- tests/employees/new_employee_test.py
+++ tests/employees/new_employee_test.py
@@ -1,14 +1,22 @@
 import pytest
 import unittest
 from pages.employees.new_employee import NewEmployee
+from utilities.read_data import getCVSData
+from ddt import ddt,data,unpack
 
 
 @pytest.mark.usefixtures("oneTimeSetUp", "setUp")
+@ddt
 class TestNewEmployee(unittest.TestCase):
     @pytest.fixture(autouse=True)
     def classSetup(self, oneTimeSetUp):
         self.nemp = NewEmployee(self.driver)
 
     @pytest.mark.run(order=1)
-    def test_NewEmployee(self):
-        self.nemp.NewEmployeeSmoke()
+    @data(*getCVSData("/home/bilalikram/PycharmProjects/Automation_Using_Database/newEmployee.csv"))
+    @unpack
+    def test_NewEmployee(self, newemp, name, newemp1, lname, newemp2, empcard, newemp3, gender, newemp4, doj, newemp5,
+                         depart, newemp6, design, newemp7, report, newemp8, tax, newemp9, urole, newemp10, policy):
+        self.nemp.NewEmployeeSmoke(newemp, name, newemp1, lname, newemp2, empcard, newemp3, gender, newemp4, doj,
+                                   newemp5, depart, newemp6, design, newemp7, report, newemp8, tax, newemp9, urole,
+                                   newemp10, policy)
